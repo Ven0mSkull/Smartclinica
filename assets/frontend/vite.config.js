a368bd9d8d7a03d6db156b85e81e60c6c51adcf8
Ven0mSkull <167183884+Ven0mSkull@users.noreply.github.com>
2025-06-26 09:57:26 +0100

    recomeco

--- assets/frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-service.hmr.overlay = false; // Desabilita o overlay de erros do Vite
